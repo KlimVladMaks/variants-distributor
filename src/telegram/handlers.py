0e8f185b1087ef55b3dbcf93b4515b6be3763e9f
vlad <vkm.blog@yandex.ru>
2026-02-13 16:58:13 +0300

    Commit 14

--- src/telegram/handlers.py
+++ src/telegram/handlers.py
@@ -6,7 +6,6 @@ from aiogram.fsm.context import FSMContext
 from .keyboards import (
     CommonKeyboards as CK,
     TeacherKeyboards as TK,
-    remove_keyboard,
 )
 from .button_text import ButtonText as BT
 from .states import Common, Teacher
@@ -42,8 +41,7 @@ async def teacher_auth(message: Message, state: FSMContext, is_init=False):
         else:
             await message.answer(
                 "Неверный пароль. " \
-                "Попробуйте ввести верный пароль или вернитесь назад.",
-                reply_markup=CK.back_kb()
+                "Попробуйте ввести верный пароль или вернитесь назад."
             )
 
 
@@ -58,16 +56,67 @@ async def teacher_main_menu(message: Message, state: FSMContext, is_init=False):
     
     elif message.text == BT.EXIT:
         await message.answer(
-            "Вы вышли из роли преподавателя.",
-            reply_markup=remove_keyboard
+            "Вы вышли из роли преподавателя."
         )
         await state.set_state(Common.choosing_role_st)
         await choosing_role(message, state, is_init=True)
+    
+    elif message.text == BT.STUDENTS_AND_FLOWS:
+        await state.set_state(Teacher.students_menu_st)
+        await teacher_students_menu(message, state, is_init=True)
     
     else:
         await message.answer(
             "Команда не распознана. Выберите интересующий вас раздел:",
             reply_markup=TK.main_menu_kb()
+        )
+
+
+@router.message(StateFilter(Teacher.students_menu_st))
+async def teacher_students_menu(message: Message, 
+                                state: FSMContext, 
+                                is_init=False):
+    """Меню преподавателя 'Студенты и потоки'"""
+    if is_init:
+        await message.answer(
+            "Студенты и потоки. Выберите интересующий вас раздел:",
+            reply_markup=TK.students_menu_kb()
+        )
+    
+    elif message.text == BT.BACK:
+        await state.set_state(Teacher.main_menu_st)
+        await teacher_main_menu(message, state, is_init=True)
+
+    elif message.text == BT.ADD_STUDENTS:
+        await state.set_state(Teacher.add_students_menu_st)
+        await teacher_add_students_menu(message, state, is_init=True)
+    
+    else:
+        await message.answer(
+            "Команда не распознана. Выберите интересующий вас раздел:",
+            reply_markup=TK.students_menu_kb()
+        )
+
+
+@router.message(StateFilter(Teacher.add_students_menu_st))
+async def teacher_add_students_menu(message: Message, 
+                                    state: FSMContext, 
+                                    is_init=False):
+    """Меню преподавателя 'Добавить студентов'"""
+    if is_init:
+        await message.answer(
+            "Как вы хотите добавить студентов?",
+            reply_markup=TK.add_students_menu_kb()
+        )
+    
+    elif message.text == BT.BACK:
+        await state.set_state(Teacher.students_menu_st)
+        await teacher_students_menu(message, state, is_init=True)
+    
+    else:
+        await message.answer(
+            "Команда не распознана. Как вы хотите добавить студентов?",
+            reply_markup=TK.add_students_menu_kb()
         )
 
 
@@ -89,8 +138,7 @@ async def choosing_role(message: Message, state: FSMContext, is_init=False):
     
     elif message.text == BT.STUDENT:
         await message.answer(
-            "Данный раздел находится в разработке. Выберите другую роль:",
-            reply_markup=CK.choosing_role_kb()
+            "Данный раздел находится в разработке. Выберите другую роль:"
         )
     
     else:
