37a7a51a8e470a5323782221d2f69f9a4d8233f8
vlad <vkm.blog@yandex.ru>
2026-02-19 22:43:20 +0300

    Commit 44

---
+++ src/devtools/tools.py
@@ -0,0 +1,25 @@
+# Запуск: python3 -m src.devtools.tools
+
+import asyncio
+from sqlalchemy import update
+
+from ..database.database import AsyncSession
+from ..database.models import Student
+
+
+async def reset_all_chat_id():
+    async with AsyncSession() as session:
+        await session.execute(
+            update(Student)
+            .values(chat_id=None)
+        )
+        await session.commit()
+    print("Поле 'chat_id' у всех студентов сброшено.")
+
+
+async def main():
+    await reset_all_chat_id()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
